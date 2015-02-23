975840fc7c027ee0133bd97bb993693be247b3d5
tjoene <jeroenbehaegel@hotmail.com>
2015-02-23 15:35:42 +0100

    Reordering of the folders

---
+++ Case_Setak/project/Build.scala
@@ -0,0 +1,86 @@
+import sbt._
+import sbt.Keys._
+
+/**
+ * Build script for test project.
+ * 
+ * @author Jeroen Behaegel (jeroen.behaegel@student.kuleuven.be)
+ */
+
+ // Build and Project settings
+object BuildSettings {
+    val projectName       = "Setak"
+    val buildOrg          = "org.kuleuven"
+    val buildOrgName      = "KU Leuven, EP"
+    val buildOrgURL       = "http://ep.khbo.be/"
+    val buildDesc         = "Testing the case(s) with Setak."
+    val buildVersion      = "1.0"
+    val buildScalaVersion = "2.9.2"
+    val buildJavaVersion  = "1.7"
+
+    val buildSettings = Seq(
+        organization         := buildOrg,
+        organizationName     := buildOrgName,
+        organizationHomepage := Some(url(buildOrgURL)),
+        description          := buildDesc,
+        version              := buildVersion,
+        scalaVersion         := buildScalaVersion,
+        shellPrompt          := ShellPrompt.buildShellPrompt
+    )
+}
+
+// Alters the shell prompt to show the current project and build version
+object ShellPrompt {
+    object devnull extends ProcessLogger {
+        def info(s: => String) {}
+        def error(s: => String) {}
+        def buffer[T](f: => T): T = f
+    }
+
+    val buildShellPrompt = { 
+        (state: State) => {
+            val currProject = Project.extract(state).currentProject.id
+            "%s:%s > ".format(currProject, BuildSettings.buildVersion)
+        }
+    }
+}
+
+// Resolvers for looking up dependencies
+object Resolvers {
+    val typesafe = "Typesafe Repo" at "http://repo.typesafe.com/typesafe/releases/"
+    val akka     = "Akka Repo" at "http://repo.akka.io/releases/"
+      
+    val myResolvers = Seq(typesafe, akka)
+}
+
+// The dependencies that are needed for the project
+object Dependencies {
+    val setak = "edu.illinois" %% "setak" % "1.0-SNAPSHOT"
+
+    val deps = Seq(setak)
+}
+
+// The actual build script, nothing should be changed in here
+object BuildScript extends Build {
+    import Resolvers._
+    import Dependencies._
+    import BuildSettings._
+
+    lazy val proj = Project (
+        id = BuildSettings.projectName,
+        base = file ("."),
+        settings = buildSettings ++ Seq(
+            resolvers := myResolvers ,
+            libraryDependencies ++= myDepencencies,
+
+            // Execute tests in the current project serially
+            parallelExecution in Test := false,
+
+            // append several options to the list of options passed to the Java compiler
+            javacOptions ++= Seq("-source", BuildSettings.buildJavaVersion, "-target", BuildSettings.buildJavaVersion),
+
+            // append -deprecation to the options passed to the Scala compiler
+            scalacOptions += "-deprecation"
+        )
+    )
+}
--- README.md
+++ README.md
@@ -3,16 +3,39 @@
 ## Table of contents
 
 - [Quick start](#quick-start)
+- [What's included](#whats-included)
 - [Versions](#versions)
 
 ## Quick start
 
 - Install [Simple Build Tool](http://www.scala-sbt.org/). At the time of writing, version 0.13.7 was used
 - Clone the repo: `git clone https://github.com/Tjoene/thesis.git`.
-- Copy the depencenies under `/DEPENDENCIES` to `C:\Users\<USER>\.ivy2\local\` for Windows, or `<HOME>/.ivy2/local/` for Linux
+- Copy the depencenies under `/Dependencies` to `C:\Users\<USER>\.ivy2\local\` for Windows, or `<HOME>/.ivy2/local/` for Linux
 - Download the depencenies into the project: `sbt update`.
 - Compile the source: `sbt compile`
 - Run the included tests `sbt test`
+
+## What's included
+
+In this repo you'll serveral folders:
+
+```
+thesis/
+├── Case_Bita/
+├── Case_Setak/
+├── Case_TestKit/
+│
+├── Dependencies/
+└── Tools/
+```
+
+In the `Case_*` folders you'll find (serveral) testcase(s) written to see the tested tool is able to find the race conditions we are looking for.
+These testcase(s) can vary from very simple to more complex programs.
+
+The `Dependencies` folder contains the pre-compiled jars of the tools that are needed to be included in the cases.
+These jars should be placed in the local Ivy-repository on your computer.
+
+The `Tools` folder contains a copy of the source code for the tools that were used during testing.  
 
 ## Versions
 
@@ -21,4 +44,4 @@ Altough the goal was to use the latest version (Scala 2.11.5 and Akka 2.3.9), bu
 - [Scala](http://scala-lang.org/) version 2.9.2
 - [Akka Actor](http://akka.io/) version 2.0.3
 
-The program `Setak` has an exception, that uses Akka Actor 1.x
+The program `Setak` is an exception, it uses Akka Actor 1.x
