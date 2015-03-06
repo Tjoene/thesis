9e6da20a4c0f6b22395703bc888e0217936e68b0
Jeroen Behaegel <jeroenbehaegel@hotmail.com>
2015-03-06 17:13:08 +0100

    Attempt to test Signal-Collec

--- Tools/signal-collect/Build.sbt
+++ Tools/signal-collect/Build.sbt
@@ -1,7 +1,3 @@
-import AssemblyKeys._ 
-
-assemblySettings
-
 /** Project */
 name := "signal-collect-core"
 
@@ -15,15 +11,27 @@ parallelExecution in Test := false
 
 resolvers += "Typesafe Snapshot Repository" at "http://repo.typesafe.com/typesafe/snapshots/"
 
+resolvers += "Akka Repo" at "http://repo.akka.io/snapshots/"
+
+resolvers += "OSS Sonatype" at "https://oss.sonatype.org/content/repositories/snapshots/"
+
+resolvers += "Typesafe Release Repository" at "http://repo.typesafe.com/typesafe/releases/"
+
+resolvers += "Akka Repo" at "http://repo.akka.io/releases/"
+
+resolvers += "OSS Sonatype" at "https://oss.sonatype.org/content/repositories/releases/"
+
 /** Dependencies */
 libraryDependencies ++= Seq(
- "com.typesafe.akka" % "akka-actor" % "2.0.3" ,
+ "com.typesafe.akka" % "akka-actor" % "2.0.3",
  "com.typesafe.akka" % "akka-remote" % "2.0.3" ,
  "org.scala-lang" % "scala-library" % "2.0.3" % "compile",
- "com.esotericsoftware.kryo" % "kryo" % "2.21-SNAPSHOT" % "compile", 
+ "com.esotericsoftware.kryo" % "kryo" % "2.21" % "compile", 
  "ch.ethz.ganymed" % "ganymed-ssh2" % "build210"  % "compile",
  "commons-codec" % "commons-codec" % "1.7"  % "compile",
  "junit" % "junit" % "4.8.2"  % "test",
  "org.specs2" %% "specs2" % "1.12.1"  % "test",
- "org.mockito" % "mockito-all" % "1.9.0"  % "test"
+ "org.mockito" % "mockito-all" % "1.9.0"  % "test",
+ "org.scalatest" %% "scalatest" % "2.0.M5b" % "test",
+ "cs.edu.uiuc" %% "bita" % "0.1" % "test"
   )
---
+++ Tools/signal-collect/src/test/scala/com/signalcollect/features/BitaSpec.scala
@@ -0,0 +1,135 @@
+package com.signalcollect.features
+
+import org.specs2.mutable._
+import org.junit.runner.RunWith
+import org.specs2.runner.JUnitRunner
+import org.specs2.matcher.Matcher
+import org.specs2.mock.Mockito
+import com.signalcollect.interfaces._
+import java.util.Map.Entry
+import com.signalcollect._
+import com.signalcollect.examples.PageRankVertex
+import com.signalcollect.examples.PageRankEdge
+import com.signalcollect.examples.SudokuCell
+import com.signalcollect.configuration.ExecutionMode
+import com.signalcollect.configuration.TerminationReason
+
+
+import akka.actor.{ ActorSystem, Actor, Props, ActorRef }
+import akka.bita.{ RandomScheduleHelper, Scheduler }
+import akka.bita.pattern.Patterns._
+import akka.util.duration._
+import akka.util.Timeout
+import akka.dispatch.Await
+
+import bita.util.{ FileHelper, TestHelper }
+
+import bita.criteria._
+import bita.ScheduleOptimization._
+import org.scalatest._
+
+
+class FunSpec extends FunSuite with TestHelper with Mockito {
+
+    // feel free to change these parameters to test the bank with various configurations.
+    def name = "SignalCollect"
+
+    implicit val timeout = Timeout(5000.millisecond)
+
+    // delay between start and end message
+    def delay = 1000
+
+    // Available criterions in Bita: PRCriterion, PCRCriterion, PMHRCriterion 
+    val criteria = Array[Criterion](PRCriterion, PCRCriterion, PMHRCriterion)
+
+    // folders where we need to store the test results
+    var allTracesDir = "test-results/%s/".format(this.name)
+    var randomTracesDir = allTracesDir+"random/"
+    var randomTracesTestDir = allTracesDir+"random-test/"
+
+    var generatedSchedulesNum = -1
+
+    // This test will keep on generating random schedules for 300 seconds until an bug is trigger. 
+    test("Test randomly within a timeout") {
+        testRandomByTime(name, randomTracesTestDir, 300) // 300 sec timeout
+    }
+
+    // Generates a random trace which will be used for schedule generation.
+    test("Generate a random trace") {
+        FileHelper.emptyDir(randomTracesDir)
+        var traceFiles = FileHelper.getFiles(randomTracesDir, (name => name.contains("-trace.txt")))
+        var traceIndex = traceFiles.length + 1
+        var newTraceName = name+"-random%s-trace.txt".format(traceIndex)
+        testRandom(name, randomTracesDir, 1)
+    }
+
+    test(" Generate and test schedules with criterion") {
+        var randomTrace = FileHelper.getFiles(randomTracesDir, (name => name.contains("-trace.txt")))
+        for (criterion <- criteria) {
+            for (opt <- criterion.optimizations.-(NONE)) {
+                var scheduleDir = allTracesDir+"%s-%s/".format(criterion.name, opt)
+
+                FileHelper.emptyDir(scheduleDir)
+                generateAndTestGeneratedSchedules(name, randomTrace, scheduleDir, criterion, opt, -1)
+            }
+        }
+    }
+
+    // This will count how many bugs there were found with a certain schedule.
+    // Giving you an indication of how good a shedule is.
+    test("Measure the coverage of testing with schedules") {
+        // The number of traces after which the coverage should be measured.
+        var interval = 5
+        for (criterion <- criteria) {
+            for (opt <- criterion.optimizations.-(NONE)) {
+                var scheduleDir = allTracesDir+"%s-%s/".format(criterion.name, opt)
+                var randomTraces = FileHelper.getFiles(randomTracesDir, (name => name.contains("-trace.txt")))
+                FileHelper.copyFiles(randomTraces, scheduleDir)
+
+                var resultFile = scheduleDir+"%s-%s-result.txt".format(criterion.name, opt)
+                var traceFiles = FileHelper.getFiles(scheduleDir, (name => name.contains("-trace.txt")))
+                traceFiles = FileHelper.sortTracesByName(traceFiles, "-%s-")
+                criterion.measureCoverage(traceFiles, resultFile, interval)
+            }
+        }
+    }
+
+    def createCircleGraph(vertices: Int): Graph = {
+        val graph = GraphBuilder.build
+        val idSet = (1 to vertices).toSet
+        for (id <- idSet) {
+            graph.addVertex(new PageRankVertex(id))
+        }
+        for (id <- idSet) {
+            graph.addEdge(id, new PageRankEdge((id % vertices) + 1))
+        }
+        graph
+    }
+
+    def run {
+        system = ActorSystem("ActorSystem")
+        RandomScheduleHelper.setSystem(system)
+
+        val graph = createCircleGraph(30)
+        val terminationCondition = new GlobalTerminationCondition(new SumOfStates[Double], 1) {
+            def shouldTerminate(sum: Option[Double]): Boolean = {
+                sum.isDefined && sum.get > 20.0 && sum.get < 29.0
+            }
+        }
+        val execConfig = ExecutionConfiguration
+            .withSignalThreshold(0)
+            .withGlobalTerminationCondition(terminationCondition)
+            .withExecutionMode(ExecutionMode.Synchronous)
+        val info = graph.execute(execConfig)
+        val state = graph.forVertexWithId(1, (v: PageRankVertex) => v.state)
+        val aggregate = graph.aggregate(new SumOfStates[Double]).get
+        
+        if (aggregate > 20.0 && aggregate < 29.0 && info.executionStatistics.terminationReason == TerminationReason.GlobalConstraintMet) {
+            bugDetected = false
+            println(Console.GREEN + Console.BOLD+"Wow. You didn't fail." + Console.RESET)
+        } else {
+            bugDetected = true
+            println(Console.RED + Console.BOLD+"You failed, not big surprise" + Console.RESET)
+        }
+    }
+}
