7bb972c2e03de5333238ae88f787e6d0a990699b
Jeroen Behaegel <jeroenbehaegel@hotmail.com>
2015-03-08 09:08:40 +0100

    Using testkit probes to read out the result, should eliminate the risk of test failure when it waits for result and it doesn't come

---
+++ Case_Bita/src/main/scala/bank5/Account.scala
@@ -0,0 +1,64 @@
+package bank5
+
+import akka.actor.{ ActorSystem, Actor, Props, ActorRef }
+
+case object Balance
+case object Continue
+case class Transfer(dest: ActorRef, amount: Int)
+case class Withdraw(amount: Int)
+case class Deposit(amount: Int, from: String)
+
+object Account {
+    def props(): Props = Props(new Account("Dummy", 0, null, null))
+    def apply(): Props = Props(new Account("Dummy", 0, null, null))
+
+    def props(holder: String, amount: Int, bank: ActorRef, next: ActorRef): Props = Props(new Account(holder, amount, bank, next))
+    def apply(holder: String, amount: Int, bank: ActorRef, next: ActorRef): Props = Props(new Account(holder, amount, bank, next))
+}
+
+class Account(var holder: String, var amount: Int, var bank: ActorRef, var next: ActorRef) extends Actor {
+
+    def receive = {
+        case Transfer(dest, amount) => { // Transfer an amount to the given destination
+            println(Console.YELLOW + Console.BOLD+"%s: sends an amount of %d".format(holder, amount) + Console.RESET)
+
+            this.amount -= amount
+            dest ! Deposit(amount, this.holder)
+        }
+
+        case Withdraw(amount) => {
+            println(Console.YELLOW + Console.BOLD+"%s: withdrawing an amount of %d".format(holder, amount) + Console.RESET)
+            this.amount -= amount
+        }
+
+        case Deposit(amount, from) => { // Deposit an incoming transfer
+            println(Console.YELLOW + Console.BOLD+"%s: received an amount of %d from %s".format(holder, amount, from) + Console.RESET)
+            this.amount += amount
+
+            if (next != null) { // only send the message if we have a target account to send our money to.
+                self ! Continue
+            }
+        }
+
+        case Continue => {
+            if (this.amount > 0) {
+                println(Console.YELLOW + Console.BOLD+"*CONTINUE* %s: amount that remains is %d".format(holder, amount) + Console.RESET)
+                next ! Deposit(1, this.holder)
+                this.amount -= 1;
+
+                self ! Continue
+            } else if (bank != null) {
+                println(Console.YELLOW + Console.BOLD+"*FINISH* %s: we are finished here, notifing the bank".format(holder) + Console.RESET)
+
+                bank ! Finish
+            }
+        }
+
+        case Balance => { // Give the current balance
+            println(Console.YELLOW + Console.BOLD+"%s: someone requested the amount of this account, which is %d".format(holder, amount) + Console.RESET)
+            sender ! this.amount
+        }
+
+        case _ => println(Console.YELLOW + Console.BOLD+"%s: 'FATAL ERROR'".format(holder) + Console.RESET)
+    }
+}
