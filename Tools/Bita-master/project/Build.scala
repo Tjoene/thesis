64ed83969e1b42daa3441f0dcebecd610e44d2e8
Jeroen Behaegel <jeroenbehaegel@hotmail.com>
2015-03-09 10:18:42 +0100

    Fix a missing dependency

--- Tools/Bita-master/project/Build.scala
+++ Tools/Bita-master/project/Build.scala
@@ -18,7 +18,7 @@ object SetakBuild extends Build {
       version := "0.1",
       scalaVersion := "2.10.4",
       resolvers += "Typesafe Repo" at "http://repo.typesafe.com/typesafe/releases/",
-      libraryDependencies += "com.typesafe.akka" % "akka-actor" % "2.3.9",
+      libraryDependencies += "com.typesafe.akka" %% "akka-actor" % "2.3.9",
       libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.4",
 
  
