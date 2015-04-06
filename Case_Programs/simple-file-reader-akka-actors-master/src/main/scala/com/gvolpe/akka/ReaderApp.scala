e0c786b0262c88da7c50e065e159c5a1bca03a7d
Jeroen Behaegel <jeroenbehaegel@hotmail.com>
2015-04-06 14:05:12 +0200

    Converting akka ask pattern to bita's version

--- Case_Programs/simple-file-reader-akka-actors-master/src/main/scala/com/gvolpe/akka/ReaderApp.scala
+++ Case_Programs/simple-file-reader-akka-actors-master/src/main/scala/com/gvolpe/akka/ReaderApp.scala
@@ -3,19 +3,19 @@ package com.gvolpe.akka
 import actors._
 import akka.actor.{ ActorSystem, Props }
 import akka.dispatch.ExecutionContexts._
-import akka.pattern.ask
+import akka.pattern.Patterns.ask
 import akka.util.Timeout
 import akka.util._
 import akka.util.duration._
 
 object ReaderApp extends App {
 
-  implicit val timeout = Timeout(25 seconds)
+  implicit val timeout = Timeout(2 seconds)
 
   val system = ActorSystem("FileReaderSystem")
   val actor = system.actorOf(WordCounterActor.props("files/rockbands.txt"))
 
-  val futureCount = actor ? StartProcessFileMsg
+  val futureCount = ask(actor, StartProcessFileMsg, timeout.duration)
 //  actor ? StartProcessFileMsg // Will print that Process is already running!
 //	actor ? StartProcessFileMsg // Will print that Process is already running!
 
