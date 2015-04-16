f4829579f886f87a363192a6cc5cc130f56afd71
Jeroen Behaegel <jeroenbehaegel@hotmail.com>
2015-04-16 10:02:50 +0200

    Don't comment out the master, comment the test.

--- Case_Bita/src/main/scala/pi/Master.scala
+++ Case_Bita/src/main/scala/pi/Master.scala
@@ -1,38 +1,38 @@
-// package pi
+package pi
 
-// import akka.actor.{ ActorRef, Props, Actor }
-// import akka.routing.RoundRobinRouter
-// import akka.util.duration._
+import akka.actor.{ ActorRef, Props, Actor }
+import akka.routing.RoundRobinRouter
+import akka.util.duration._
 
-// // Use master.prop in the code or Master()
-// // See http://doc.akka.io/docs/akka/snapshot/scala/actors.html#Recommended_Practices
-// object Master {
-//     def props(nrOfWorkers: Int, nrOfElements: Int, nrOfMessages: Int, listener: ActorRef): Props =
-//         Props(new Master(nrOfWorkers, nrOfElements, nrOfMessages, listener))
+// Use master.prop in the code or Master()
+// See http://doc.akka.io/docs/akka/snapshot/scala/actors.html#Recommended_Practices
+object Master {
+    def props(nrOfWorkers: Int, nrOfElements: Int, nrOfMessages: Int, listener: ActorRef): Props =
+        Props(new Master(nrOfWorkers, nrOfElements, nrOfMessages, listener))
 
-//     def apply(nrOfWorkers: Int, nrOfElements: Int, nrOfMessages: Int, listener: ActorRef): Props =
-//         Props(new Master(nrOfWorkers, nrOfElements, nrOfMessages, listener))
-// }
+    def apply(nrOfWorkers: Int, nrOfElements: Int, nrOfMessages: Int, listener: ActorRef): Props =
+        Props(new Master(nrOfWorkers, nrOfElements, nrOfMessages, listener))
+}
 
-// class Master(nrOfWorkers: Int, nrOfMessages: Int, nrOfElements: Int, listener: ActorRef)
-//         extends Actor {
+class Master(nrOfWorkers: Int, nrOfMessages: Int, nrOfElements: Int, listener: ActorRef)
+        extends Actor {
 
-//     var pi: Double = _
-//     var nrOfResults: Int = _
-//     val start: Long = System.currentTimeMillis
+    var pi: Double = _
+    var nrOfResults: Int = _
+    val start: Long = System.currentTimeMillis
 
-//     val workerRouter = context.actorOf(Worker().withRouter(RoundRobinRouter(nrOfWorkers)), name = "workerRouter")
+    val workerRouter = context.actorOf(Worker().withRouter(RoundRobinRouter(nrOfWorkers)), name = "workerRouter")
 
-//     def receive = {
-//         case Calculate =>
-//             for (i <- 0 until nrOfMessages) workerRouter ! Work(i * nrOfElements, nrOfElements)
-//         case Result(value) =>
-//             pi += value
-//             nrOfResults += 1
-//             if (nrOfResults == nrOfMessages) {
-//                 //listener ! PiApproximation(pi, duration = (System.currentTimeMillis - start).millis)
-//                 listener ! pi
-//                 context.stop(self)
-//             }
-//     }
-// }
+    def receive = {
+        case Calculate =>
+            for (i <- 0 until nrOfMessages) workerRouter ! Work(i * nrOfElements, nrOfElements)
+        case Result(value) =>
+            pi += value
+            nrOfResults += 1
+            if (nrOfResults == nrOfMessages) {
+                //listener ! PiApproximation(pi, duration = (System.currentTimeMillis - start).millis)
+                listener ! pi
+                context.stop(self)
+            }
+    }
+}
